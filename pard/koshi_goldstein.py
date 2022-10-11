bbc95f7387a0f4b5cad3e7d1cd0022279432479f
JasonMendoza2008 <lhotteromain@gmail.com>
2022-10-11 15:28:03 +0200

    Preparing the 15 Koshi-Goldstein matrices
    Signed-off-by: JasonMendoza2008 <lhotteromain@gmail.com>

--- pard/koshi_goldstein.py
+++ pard/koshi_goldstein.py
@@ -1,18 +1,38 @@
+import enum
 import logging
 
 import pard.raw_python_dictionaries
 from pard.handling_3_letter_code_decorator import handling_3_letter_code
 
+
+class MatrixType(Enum):
+    ALL_RESIDUES = 1
+    HELIX = 2
+    SHEET = 3
+    EXPOSED = 4
+    BURIED = 5
+    TURN = 6
+    COIL = 7
+    BURIED_HELIX = 8
+    BURIED_SHEET = 9
+    BURIED_TURN = 10
+    BURIED_COIL = 11
+    EXPOSED_TURN = 12
+    EXPOSED_COIL = 13
+
+
 @handling_3_letter_code
 def koshi_goldstein(
         amino_acid_1: str,
         amino_acid_2: str,
+        matrix_type: MatrixType,
         symmetric: bool,
         warning: bool = True,
 ) -> float:
     """
     :param amino_acid_1: 1 letter code of the first amino acid
     :param amino_acid_2: 1 letter code of the third amino acid
+    :param matrix_type: which optimal Koshi-Goldstein substitution matrix is desired
     :param symmetric: whether the symmetric version of the matrix is wanted or not (i.e. exchanges of known* or
                       unknown direction)
                       * amino_acid_1 -> amino_acid_2
