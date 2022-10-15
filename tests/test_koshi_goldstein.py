7ff7466a2fd2a332da1de8b2e07e6c94823a6242
JasonMendoza2008 <lhotteromain@gmail.com>
2022-10-15 19:58:03 +0200

    Added unit tests
    Signed-off-by: JasonMendoza2008 <lhotteromain@gmail.com>

--- tests/test_koshi_goldstein.py
+++ tests/test_koshi_goldstein.py
@@ -12,6 +12,10 @@ def test_koshi_goldstein_all_residues() -> None:
     # Symmetric tests
     assert koshi_goldstein("C", "W", MatrixType.ALL_RESIDUES, True, warning=False) == pytest.approx((0.2+0.7)/2)
     assert koshi_goldstein("W", "C", MatrixType.ALL_RESIDUES, True, warning=False) == pytest.approx((0.7+0.2)/2)
+    assert koshi_goldstein("R", "W", MatrixType.ALL_RESIDUES, True, warning=False) == pytest.approx((0.3+0.4)/2)
+    assert koshi_goldstein("W", "R", MatrixType.ALL_RESIDUES, True, warning=False) == pytest.approx((0.4+0.3)/2)
+    assert koshi_goldstein("R", "-", MatrixType.ALL_RESIDUES, True, warning=False) == pytest.approx((11.3+2.0)/2)
+    assert koshi_goldstein("-", "R", MatrixType.ALL_RESIDUES, True, warning=False) == pytest.approx((2.0+11.3)/2)
 
     # Asymmetric tests
     assert koshi_goldstein("C", "W", MatrixType.ALL_RESIDUES, False, warning=False) == 0.2
@@ -20,6 +24,14 @@ def test_koshi_goldstein_all_residues() -> None:
     assert koshi_goldstein("W", "C", MatrixType.ALL_RESIDUES, False, warning=False) == 0.7
     assert koshi_goldstein("TRP", "CYS", MatrixType.ALL_RESIDUES, False, warning=False) == 0.7
     assert koshi_goldstein("W", "CYS", MatrixType.ALL_RESIDUES, False, warning=False) == 0.7
+    assert koshi_goldstein("R", "W", MatrixType.ALL_RESIDUES, False, warning=False) == 0.3
+    assert koshi_goldstein("Arg", "Trp", MatrixType.ALL_RESIDUES, False, warning=False) == 0.3
+    assert koshi_goldstein("arg", "trp", MatrixType.ALL_RESIDUES, False, warning=False) == 0.3
+    assert koshi_goldstein("W", "R", MatrixType.ALL_RESIDUES, False, warning=False) == 0.4
+    assert koshi_goldstein("TRP", "ARG", MatrixType.ALL_RESIDUES, False, warning=False) == 0.4
+    assert koshi_goldstein("W", "ARG", MatrixType.ALL_RESIDUES, False, warning=False) == 0.4
+    assert koshi_goldstein("R", "-", MatrixType.ALL_RESIDUES, False, warning=False) == 11.3
+    assert koshi_goldstein("-", "R", MatrixType.ALL_RESIDUES, False, warning=False) == 2.0
 
     # With warning; tests still go through?
     # All Residues
@@ -37,6 +49,22 @@ def test_koshi_goldstein_exposed_residues() -> None:
 
     :return: Nothing
     """
+    # Symmetric tests
+    assert koshi_goldstein("R", "W", MatrixType.EXPOSED, True, warning=False) == pytest.approx((0.6+0.1)/2)
+    assert koshi_goldstein("W", "R", MatrixType.EXPOSED, True, warning=False) == pytest.approx((0.1+0.6)/2)
+    assert koshi_goldstein("R", "-", MatrixType.EXPOSED, True, warning=False) == pytest.approx((11.4+1.7)/2)
+    assert koshi_goldstein("-", "R", MatrixType.EXPOSED, True, warning=False) == pytest.approx((1.7+11.4)/2)
+
+    # Asymmetric tests
+    assert koshi_goldstein("R", "W", MatrixType.EXPOSED, False, warning=False) == 0.6
+    assert koshi_goldstein("Arg", "Trp", MatrixType.EXPOSED, False, warning=False) == 0.6
+    assert koshi_goldstein("arg", "trp", MatrixType.EXPOSED, False, warning=False) == 0.6
+    assert koshi_goldstein("W", "R", MatrixType.EXPOSED, False, warning=False) == 0.1
+    assert koshi_goldstein("TRP", "ARG", MatrixType.EXPOSED, False, warning=False) == 0.1
+    assert koshi_goldstein("W", "ARG", MatrixType.EXPOSED, False, warning=False) == 0.1
+    assert koshi_goldstein("R", "-", MatrixType.EXPOSED, False, warning=False) == 11.4
+    assert koshi_goldstein("-", "R", MatrixType.EXPOSED, False, warning=False) == 1.7
+
     # With warning; tests still go through?
     # Exposed Residues
     assert koshi_goldstein("-", "-", MatrixType.EXPOSED, False, warning=True) == 60.2
