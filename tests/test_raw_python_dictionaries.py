bc42075b330435488c6ef02e79b4530eca019c62
JasonMendoza2008 <lhotteromain@gmail.com>
2022-11-28 15:18:53 +0100

    Bugfix Koshi-Goldstein
    Signed-off-by: JasonMendoza2008 <lhotteromain@gmail.com>

--- tests/test_raw_python_dictionaries.py
+++ tests/test_raw_python_dictionaries.py
@@ -19,12 +19,18 @@ def test_length_dict() -> None:
     asymmetric_experimental_exchangeability_dict: dict[tuple[str, str], int | None]
     grantham_dict: dict[tuple[str, str], int]
 
+    ## Getting dicts
+    # Sneath dict
     sneath_dict                                  = make_sneath_dict()
+    # Miyata dict
     miyata_dict                                  = make_miyata_dict()
+    # Epstein dicts
     symmetric_epstein_dict                       = make_symmetric_epstein_dict()
     asymmetric_epstein_dict                      = make_asymmetric_epstein_dict()
+    # Experimental exchangeability dicts
     symmetric_experimental_exchangeability_dict  = make_symmetric_experimental_exchangeability_dict()
     asymmetric_experimental_exchangeability_dict = make_asymmetric_experimental_exchangeability_dict()
+    # Grantham dict
     grantham_dict                                = make_grantham_dict()
     # Koshi-Goldstein dicts
     (
@@ -38,6 +44,7 @@ def test_length_dict() -> None:
         asymmetric_koshi_goldstein_buried_residues_dict,
     ) = make_asymmetric_koshi_golstein_dicts()
 
+    ## 20x20 matrices
     assert(len(sneath_dict) == 400)
     assert(len(miyata_dict) == 400)
     assert(len(symmetric_epstein_dict) == 400)
@@ -45,6 +52,7 @@ def test_length_dict() -> None:
     assert(len(symmetric_experimental_exchangeability_dict) == 400)
     assert(len(asymmetric_experimental_exchangeability_dict) == 400)
     assert(len(grantham_dict) == 400)
+    ## 21x21 matrices
     # Koshi-Goldstein matrices
     assert(len(symmetric_koshi_goldstein_all_residues_dict) == 441)
     assert(len(symmetric_koshi_goldstein_exposed_residues_dict) == 441)
