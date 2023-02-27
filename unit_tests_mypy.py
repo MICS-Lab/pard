a3b4c38ce7a60496196d769ddb282471c46c0a2a
JasonMendoza2008 <lhotteromain@gmail.com>
2023-02-27 17:14:32 +0100

    Better unit_tests_*.py file
    Signed-off-by: JasonMendoza2008 <lhotteromain@gmail.com>

--- unit_tests_mypy.py
+++ unit_tests_mypy.py
@@ -1,6 +1,8 @@
 import pytest
 
-retcode: int | pytest.ExitCode = pytest.main(["--mypy"])
 
-if retcode == pytest.ExitCode.OK:
-    print("\n--- \nAll good.\n--- \n")
+if __name__ == "__main__":
+    retcode: int | pytest.ExitCode = pytest.main(["--mypy"])
+
+    if retcode == pytest.ExitCode.OK:
+        print("\n--- \nAll good.\n--- \n")
